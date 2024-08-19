6368aa0b330eb75caf91db80509a4ace4b042c7f
nabaraj malla <nabarajmalla4000@gmail.com>
2024-08-20 04:50:00 +0545

    added email and password validation using regex

--- src/features/user/user.schema.js
+++ src/features/user/user.schema.js
@@ -9,8 +9,19 @@ export const userSchema = new mongoose.Schema({
   email: {
     type: String,
     unique: true,
+    match: [
+      /^([a-zA-Z0-9_\-\.]+)@test\.com$/,
+      "please enter a valid email address",
+    ],
   },
-  password: String,
+  // password: String,
+  password: {
+    type: String,
+    match: [
+      /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/,
+      "password must contain at least one uppercase letter, one lowercase letter, one digit, and one special character and minimum length of 8",
+    ],
+  },
   // address: {
   //   province: {
   //     type: String,
