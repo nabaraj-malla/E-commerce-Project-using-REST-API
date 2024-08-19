fb627e7d519bc4e47b6347bdfd9c13cd390a887c
nabaraj malla <nabarajmalla4000@gmail.com>
2024-08-20 05:22:45 +0545

    fixed user model

--- src/features/user/user.schema.js
+++ src/features/user/user.schema.js
@@ -3,25 +3,30 @@ import mongoose from "mongoose";
 export const userSchema = new mongoose.Schema({
   name: {
     type: String,
+    required: true,
     minLength: [5, "name can't be less than 5"],
     maxLength: [20, "Name can't be greater than 25 characters"],
   },
   email: {
     type: String,
+    required: true,
     unique: true,
     match: [
       /^([a-zA-Z0-9_\-\.]+)@test\.com$/,
       "please enter a valid email address",
     ],
   },
-  // password: String,
   password: {
     type: String,
-    match: [
-      /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/,
-      "password must contain at least one uppercase letter, one lowercase letter, one digit, and one special character and minimum length of 8",
-    ],
+    required: true,
   },
+  // password: {
+  //   type: String,
+  //   match: [
+  //     /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/,
+  //     "password must contain at least one uppercase letter, one lowercase letter, one digit, and one special character and minimum length of 8",
+  //   ],
+  // },
   // address: {
   //   province: {
   //     type: String,
